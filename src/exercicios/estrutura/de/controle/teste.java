9d0dc1f83a33f10afef7ba334623ff193fe1b31a
FACULDADE <FACULDADE@10.8.0.21>
2013-06-15 09:12:39 -0300

    teste

---
+++ src/exercicios/estrutura/de/controle/teste.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios.estrutura.de.controle;
+
+/**
+ *
+ * @author FACULDADE
+ */
+public class teste {
+    
+}
